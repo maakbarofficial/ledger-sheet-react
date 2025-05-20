178d913acec5c27b91fac67fc48aba08ef24eabb
Muhammad Ali Akbar <maakbarofficial@gmail.com>
2025-05-20 16:21:47 +0500

    Add Loader

---
+++ src/Loader.jsx
@@ -0,0 +1,9 @@
+const Loader = () => {
+  return (
+    <div style={{ display: "flex", alignItems: "center", justifyContent:"center", height: "100vh"}}>
+      <div className="loader"></div>
+    </div>
+  );
+};
+
+export default Loader;
